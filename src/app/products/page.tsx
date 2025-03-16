9bb20a4776dd7bf79b254974912e03559dac4cb6
ksb-dev-1 <ksbabaleshwar@gmail.com>
2025-03-16 15:56:44 +0530

    initial commit

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,11 +4,13 @@ import Container from "@/components/Container";
 export default function ProductsPage() {
   return (
     <Container>
-      <Link href="/products/1">Product 1</Link>
-      <Link href="/products/2">Product 2</Link>
-      <Link href="/products/3">Product 3</Link>
-      <Link href="/products/4">Product 4</Link>
-      <Link href="/products/5">Product 5</Link>
+      <div className="flex flex-col space-y-4">
+        <Link href="/products/1">Product 1</Link>
+        <Link href="/products/2">Product 2</Link>
+        <Link href="/products/3">Product 3</Link>
+        <Link href="/products/4">Product 4</Link>
+        <Link href="/products/5">Product 5</Link>
+      </div>
     </Container>
   );
 }
