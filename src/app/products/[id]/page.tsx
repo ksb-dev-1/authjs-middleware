455bfc18f251abebbfd5c5d51f771469299bd109
ksb-dev-1 <ksbabaleshwar@gmail.com>
2025-03-16 15:54:22 +0530

    initial commit

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,16 @@
 import Container from "@/components/Container";
 
-export default function ProductDetails() {
-  return <Container>Product Details</Container>;
+export default async function ProductDetails({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  return (
+    <Container>
+      <h1 className="font-bold text-xl mb-4">Details</h1>
+      <p>Product {id}</p>
+    </Container>
+  );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
 import Container from "@/components/Container";
 
 export default function ProductsPage() {
-  return <Container>Products Page</Container>;
+  return (
+    <Container>
+      <Link href="/products/1">Product 1</Link>
+      <Link href="/products/2">Product 2</Link>
+      <Link href="/products/3">Product 3</Link>
+      <Link href="/products/4">Product 4</Link>
+      <Link href="/products/5">Product 5</Link>
+    </Container>
+  );
 }
